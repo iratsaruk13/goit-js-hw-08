163441da36ab0cd890bff5c4e65634bed424fce5
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-03-12 21:35:36 +0200

    1 task done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items";
+
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+      <img
+      loading="lazy"
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>`;
+    })
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+// console.log(galleryEl);
+
+new SimpleLightbox('.gallery .gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
