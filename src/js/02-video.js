5ad92bd1dba457b9fb4abcc8b7b8c0fd1c7811c1
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-03-12 22:26:21 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const CURRENT_TIME = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+
+player.on(
+  'timeupdate',
+  throttle(handleTimeUpdate, 1000));
+function handleTimeUpdate(evt) {
+    localStorage.setItem(CURRENT_TIME, evt.seconds);
+}
+// console.log(player);
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
